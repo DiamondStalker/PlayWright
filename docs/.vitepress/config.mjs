72d6e94614aafb723a30d793284d10bf18ff3619
DiamondStalker <cmoreno981@gmail.com>
2023-10-12 22:26:34 -0500

    Change build dist

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/nombre-de-ruta/',
   title: "Projects",
   description: "Documentation of all the different projects with their fixes",
   themeConfig: {
